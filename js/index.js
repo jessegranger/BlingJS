3223f72590e7b0b499d61b8d3a64c14d6cdb9f9d
Jesse Granger <jesse_dailey@fastmail.com>
2017-01-16 16:43:08 -0800

    site: pare down the home page to the minimum

--- js/index.js
+++ js/index.js
@@ -5,30 +5,5 @@
 			var pkg = JSON.parse(text);
 			$("a[name=Top]").after("<sub>v"+pkg.version+"</sub>");
 		});
-	}).ready(function () {
-		// find unsupported fraction markup
-		// and insert fall-back text
-		var num_re = /&frac(\d)(\d);/;
-		$("p").each(function() {
-			var p = $(this),
-			text = p.text()[0]
-			if( num_re.test(text) )
-				p.text(text.replace(num_re, "$1/$2"))
-		})
-	})
-	// replace tabs with spaces in <pre>'s
-	// make all <pre>'s pretty-printable
-	.ready(function () {
-		var initial_tab = /(^|\n)\t/g,
-			all_tabs = /\t/g
-
-		if( $.prettyPrint )
-			$("pre").zap('innerHTML', function(html) {
-				return $.prettyPrint(html.replace(all_tabs, "  "));
-			})
-		// clean up textarea's also
-		$("textarea").zap('value', function (value) {
-			return value.replace(initial_tab,'$1').replace(all_tabs, "  ")
-		})
 	})
 })(Bling)
